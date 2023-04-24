83228be82755e6ddd74d7be6ddda38235ac6112a
ahmedmokhtar80 <ahmedmokhtar80@gmail.com>
2023-04-24 16:46:16 +0200

    Update ETAOIN_practice.py
    I am a beginner in learning Python language.
    In this script I've tried to include everything I've learned so far.

--- Chapter_1/ETAOIN_practice.py
+++ Chapter_1/ETAOIN_practice.py
@@ -1,23 +1,37 @@
 """Map letters from string into dictionary & print bar chart of frequency."""
-import sys
-import pprint
-from collections import defaultdict
+import string, sys
 
-# Note: text should be a short phrase for bars to fit in IDLE window
-text = 'Like the castle in its corner in a medieval game, I foresee terrible \
-trouble and I stay here just the same.'
+class Etaoin:
+    def __init__(self, text):
+        self.text = text
+        self.keys = ''
+        self.dict_letters = {}
+        
+        self.text_to_keys()
+        self.keys_to_dict()
+        self.count_letters()
+    
+    def text_to_keys(self):
+        dict_trans = self.text.maketrans(dict.fromkeys(string.punctuation + ' '))
+        self.keys = self.text.translate(dict_trans).lower()
 
-ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+    def keys_to_dict(self):
+        self.dict_letters = dict.fromkeys(sorted(self.keys))
 
-# defaultdict module lets you build dictionary keys on the fly!
-mapped = defaultdict(list)
-for character in text:
-    character = character.lower()
-    if character in ALPHABET:
-        mapped[character].append(character)
+    def count_letters(self):
+        for k in self.dict_letters:
+            self.dict_letters.update({k: [k] * self.keys.count(k)})
 
-# pprint lets you print stacked output
+    def __str__(self):
+        d = [str(pairs) for pairs in self.dict_letters.items()]
+        return '\n'.join(d)
+
+text = 'Like the castle in its corner in a medieval game, \
+I forsee terrible trouble and i stay here just the same.'
+
+etaoin_text = Etaoin(text)
 print("\nYou may need to stretch console window if text wrapping occurs.\n")
 print("text = ", end='')
-print("{}\n".format(text), file=sys.stderr)
-pprint.pprint(mapped, width=110)
+print(f"{text}\n", file=sys.stderr)
+print(etaoin_text)
+
