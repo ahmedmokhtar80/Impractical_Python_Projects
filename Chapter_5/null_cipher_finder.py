d68b3f5944b4ff6cc0bbeda82e300172f2d9c011
ahmedmokhtar80 <ahmedmokhtar80@gmail.com>
2023-05-14 11:59:05 +0300

    Update null_cipher_finder.py

--- Chapter_5/null_cipher_finder.py
+++ Chapter_5/null_cipher_finder.py
@@ -1,63 +1,63 @@
 """Decode a null cipher based on number of letters after punctuation marks."""
-import sys
-import string
 
-def load_text(file):
-    """Load a text file as a string."""
-    with open(file) as f:
-        return f.read().strip()
-         
-def solve_null_cipher(message, lookahead):
-    """Solve a null cipher based on number letters after punctuation mark.
+import string, sys
 
-    message = null cipher text as string stripped of whitespace
-    lookahead = endpoint of range of letters after punctuation mark to examine
-    """
-    for i in range(1, lookahead + 1):
-        plaintext = ''
-        count = 0
-        found_first = False
-        for char in message:
-            if char in string.punctuation:
-                count = 0
-                found_first = True
-            elif found_first is True:
-                count += 1
-            if count == i:
-                plaintext += char
-        print("Using offset of {} after punctuation = {}".format(i, plaintext))
-        print()
+punctuation = string.punctuation
 
 def main():
-    """Load text, solve null cipher."""
+    """Loat text, solve null cipher."""
     # load & process message:
-    filename = input("\nEnter full filename for message to translate: ")
-    try:
-        loaded_message = load_text(filename)
-    except IOError as e:
-        print("{}. Terminating program.".format(e), file=sys.stderr)
-        sys.exit(1)
+    filename = input("\nEnter filename for message to translate: ")
+    loaded_message = load_text(filename)
     print("\nORIGINAL MESSAGE =")
-    print("{}".format(loaded_message), "\n")
-    print("\nList of punctuation marks to check = {}".
-          format(string.punctuation), "\n")
-    
-    # remove whitespace:
+    print(f"{loaded_message}", "\n")
+    print(f"\nList of punctuation marks to check {punctuation}")
+
     message = ''.join(loaded_message.split())
 
-    # get range of possible cipher keys from user:
-    while True:
-        lookahead = input("\nNumber of letters to check after " \
-                          "punctuation mark: ")
-        if lookahead.isdigit():
-            lookahead = int(lookahead)
-            break
-        else:
-            print("Please input a number.", file=sys.stderr)           
-    print()
+    null_words = get_null_words(message, 3)
+    null_chipher = solve_null_cipher(null_words)
 
-    # run function to decode cipher:
-    solve_null_cipher(message, lookahead)
+    print()
+    print(null_chipher)
+
     
+def load_text(file):
+    """Load a text file as a string."""
+    try:
+        with open(file) as f:
+            return f.read().strip()
+    except IOError as e:
+        print(f'{e}.', f'Error opening {file}. Terminating program.', file=sys.stderr, sep='\n')
+        sys.exit(1)
+
+
+def get_null_words(message, count):
+    """get a null words based on number letters after punctuation mark."""
+    null_cipher = []
+    for i, letter in enumerate(message):
+        if letter in punctuation:
+            _index = message.find(letter, i)
+            null_cipher.append(message[_index+1:_index+count+1])
+    return null_cipher
+
+def solve_null_cipher(null_words):
+    """Solve a null cipher based on number letters after punctuation mark."""
+    null_cipher = ""
+    for word in null_words:
+        if word == '':
+            continue
+        no_punctuation = True
+    
+        for letter in word:
+            if letter in punctuation:
+                no_punctuation = False
+            
+        if no_punctuation:
+            null_cipher += word[-1:]
+    return null_cipher
+    
+
 if __name__ == '__main__':
     main()
+
