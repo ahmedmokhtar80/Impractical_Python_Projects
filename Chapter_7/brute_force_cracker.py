910f10ce4985ea0471db86f4803fee6a9b52d57c
ahmedmokhtar80 <ahmedmokhtar80@gmail.com>
2023-05-18 16:17:36 +0300

    Update brute_force_cracker.py

--- Chapter_7/brute_force_cracker.py
+++ Chapter_7/brute_force_cracker.py
@@ -1,16 +1,24 @@
 """Use brute force to crack a lock combination."""
-import time
-from itertools import product
+
+import time, sys
+from random import choices
 
 start_time = time.time()
 
 combo = (9, 9, 7, 6, 5)
 
-# use Cartesian product to generate permutations with repetition
-for perm in product([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], repeat=len(combo)):
+numbers = list(range(0,10))
+attempts = 0
+
+# use randint to generate permutations with repetition
+while True:
+    perm = tuple(choices(numbers, k=len(combo)))
+    attempts += 1
     if perm == combo:
-        print("Cracked! {} {}".format(combo, perm))
+        print('\nCracked! {} {}'.format(combo, perm))
+        print('it took {} attempts to crack!'.format(attempts), file=sys.stderr)
+        break
 
 end_time = time.time()
-print("\nRuntime for this program was {} seconds.".format
-      (end_time - start_time))   
+print('\nRuntime for this program was {} seconds.'
+      .format(end_time - start_time))
